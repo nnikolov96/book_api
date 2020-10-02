f190e169394fae0b3f52825d106f339b406145b4
Nikola <nikolanenchevnikolov@gmail.com>
2020-10-03 00:07:19 +0300

    trait for admin in users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     sequence(:email) { |n| "person#{n}@gmail.com" }
     password { 'Test1234#' }
     password_confirmation { 'Test1234#' }
+    admin { false }
+
+    trait :admin do
+      admin { true }
+    end
   end
 end
