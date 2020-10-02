3f9a1cd54f6b8c7156a512333128a11499137236
Nikola <nikolanenchevnikolov@gmail.com>
2020-10-02 22:43:08 +0300

    validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  validates :email, presence: true, length: { minimum: 5, maximum: 120 }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6, maximum: 26 }
   has_secure_password
 end
