2f766bdac05b651ab727effc4e0dfeb6d2d7604b
Nikola <nikolanenchevnikolov@gmail.com>
2020-10-03 15:43:50 +0300

    rating for book

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,12 @@ class Book < ApplicationRecord
   validates :title, presence: true, length: { minimum: 2, maximum: 100 }
   validates :description, length: { maximum: 1000 }
   validates :isbn, presence: true, uniqueness: true
+
+  def rating
+    if reviews.present?
+      reviews.average(:rating).round(1)
+    else
+      0.0
+    end
+  end
 end
