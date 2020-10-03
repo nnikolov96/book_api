f749546b3ce8afd42312d15595042979de639581
Nikola <nikolanenchevnikolov@gmail.com>
2020-10-03 18:37:54 +0300

    very basics seed method for books. Will be changed later

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  Book.create!(
+    title: Faker::Book.title,
+    description: Faker::Lorem.sentence(word_count: 15),
+    isbn: Faker::Lorem.characters(number: 10)
+  )
+end
