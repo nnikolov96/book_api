1dc02566306efb9eeba76246d7f6c6eff22ef593
Nikola <nikolanenchevnikolov@gmail.com>
2020-10-03 13:08:23 +0300

    associations for reviews in book and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :reviews, dependent: :destroy
   validates :email, presence: true, length: { minimum: 5, maximum: 120 }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6, maximum: 26 }
   has_secure_password
