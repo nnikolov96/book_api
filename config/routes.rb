0e82a2cf207bfb6800e3f1dc87bc0372c47881bc
Nikola <nikolanenchevnikolov@gmail.com>
2020-10-04 00:10:53 +0300

    route for search in books index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'books/search' => 'books#index'
       resources :users, only: %i[create]
       resources :tokens, only: %i[create]
       resources :books do
