38f48579749c57f16281caafe2521d48aa90c357
Nikola <nikolanenchevnikolov@gmail.com>
2020-10-05 17:24:42 +0300

    added image attachment to book

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
-
   validates :text, presence: true, length: { minimum: 20, maximum: 250 }
   validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5.0 }
 end
